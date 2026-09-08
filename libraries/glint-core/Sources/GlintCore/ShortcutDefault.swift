e8f861cfadc9798578d2107fa5ac71ef7b78335c
Broderick <8eecf0d2@gmail.com>
2026-09-09 07:16:46 +1000

    GLNT-16 Resolve packaged shortcut resources and verify all defaults

--- libraries/glint-core/Sources/GlintCore/ShortcutDefault.swift
+++ libraries/glint-core/Sources/GlintCore/ShortcutDefault.swift
@@ -10,7 +10,17 @@ public struct ShortcutDefault: Decodable, Identifiable, Sendable {
     public var shortcutBinding: ShortcutBinding? { ShortcutBinding(spectacleString: binding) }
 
     public static func load() throws -> [ShortcutDefault] {
-        guard let url = Bundle.module.url(forResource: "default-shortcuts", withExtension: "json") else {
+        let resources: Bundle
+        if Bundle.main.bundleURL.pathExtension == "app" {
+            guard let url = Bundle.main.url(forResource: "GlintCore_GlintCore", withExtension: "bundle"),
+                  let packagedResources = Bundle(url: url) else {
+                throw CocoaError(.fileNoSuchFile)
+            }
+            resources = packagedResources
+        } else {
+            resources = Bundle.module
+        }
+        guard let url = resources.url(forResource: "default-shortcuts", withExtension: "json") else {
             throw CocoaError(.fileNoSuchFile)
         }
         return try JSONDecoder().decode([ShortcutDefault].self, from: Data(contentsOf: url))
